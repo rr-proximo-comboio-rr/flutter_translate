53378db33a38415c52adcef23d2221162a8acd3c
Ricardo Rodrigues <ricardo.jorge.rodrigues.00@gmail.com>
2026-02-26 11:20:02 +0000

    fix: update example app & apply needed migration for app to run

--- lib/flutter_translate.dart
+++ lib/flutter_translate.dart
@@ -1,10 +1,10 @@
 library flutter_translate;
 
-export 'src/utils/utils.dart';
-export 'src/services/localization.dart';
 export 'src/delegates/localization_delegate.dart';
+export 'src/interfaces/translate_preferences.dart';
+export 'src/services/localization.dart';
+export 'src/utils/device_locale.dart';
+export 'src/utils/utils.dart';
 export 'src/widgets/localization_provider.dart';
 export 'src/widgets/localized_app.dart';
 export 'src/widgets/localized_app_state.dart';
-export 'src/interfaces/translate_preferences.dart';
-export 'src/utils/device_locale.dart';
--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,15 +1,14 @@
-class Constants
-{
-    static const String assetManifestFilename = 'AssetManifest.json';
+class Constants {
+  const Constants._();
 
-    static const String localizedAssetsPath = 'assets/i18n';
+  static const String localizedAssetsPath = 'assets/i18n';
 
-    static const String pluralZero = 'zero';
-    static const String pluralOne = 'one';
-    static const String pluralTwo = 'two';
-    static const String pluralFew = 'few';
-    static const String pluralMany = 'many';
-    static const String pluralOther = 'other';
+  static const String pluralZero = 'zero';
+  static const String pluralOne = 'one';
+  static const String pluralTwo = 'two';
+  static const String pluralFew = 'few';
+  static const String pluralMany = 'many';
+  static const String pluralOther = 'other';
 
-    static const String pluralValueArg = '{{value}}';
+  static const String pluralValueArg = '{{value}}';
 }
